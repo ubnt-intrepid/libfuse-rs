353e5c1e7734235a11755c9167dcfbfca3360528
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2019-08-31 16:23:22 +0900

    add open/read

--- src/main.rs
+++ src/main.rs
@@ -42,11 +42,11 @@ fn main() {
         lock: None,
         mkdir: None,
         mknod: None,
-        open: None,
+        open: Some(passthrough_open),
         opendir: None,
         poll: None,
         read_buf: None,
-        read: None,
+        read: Some(passthrough_read),
         readdir: Some(passthrough_readdir),
         readlink: Some(passthrough_readlink),
         release: None,
@@ -131,6 +131,8 @@ unsafe extern "C" fn passthrough_readlink(
     buf: *mut c_char,
     size: usize,
 ) -> c_int {
+    log::trace!("called passthrough_readlink()");
+
     let res = readlink(path, buf, size - 1);
     if res == -1 {
         return errno() * -1;
@@ -175,4 +177,50 @@ unsafe extern "C" fn passthrough_readdir(
 
     closedir(dp);
     0
+}
+
+unsafe extern "C" fn passthrough_open(path: *const c_char, fi: *mut fuse_file_info) -> c_int {
+    log::trace!("called passthrough_open()");
+
+    let fi = &mut *fi;
+
+    let res = open(path, fi.flags);
+    if res == -1 {
+        return errno() * -1;
+    }
+
+    fi.fh = res as u64;
+    0
+}
+
+unsafe extern "C" fn passthrough_read(
+    path: *const c_char,
+    buf: *mut c_char,
+    size: usize,
+    offset: off_t,
+    fi: *mut fuse_file_info,
+) -> c_int {
+    log::trace!("called passthrough_read()");
+
+    let fd;
+    if fi == ptr::null_mut() {
+        fd = open(path, O_RDONLY as i32);
+    } else {
+        let fi = &mut *fi;
+        fd = fi.fh as c_int;
+    }
+    if fd == -1 {
+        return errno() * -1;
+    }
+
+    let mut res = pread(fd, buf as *mut c_void, size, offset) as c_int;
+    if res == -1 {
+        res = errno() * -1;
+    }
+
+    if fi == ptr::null_mut() {
+        close(fd);
+    }
+
+    res
 }
